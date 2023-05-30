235f9c2ca1f61c5eefc9cd4144441424a78fa675
Lucas Vinicius <111596218+lucaolv@users.noreply.github.com>
2023-05-30 17:10:04 -0300

    Update ListarDados.java

--- Turma65/src/ExerciciosAula5/ListarDados.java
+++ Turma65/src/ExerciciosAula5/ListarDados.java
@@ -1,6 +1,7 @@
 package ExerciciosAula5;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -9,6 +10,7 @@ public class ListarDados {
 	public static void main(String[] args) {
 
 		Set<Integer> setNumeros = new HashSet<>();
+		Iterator<Integer> isetNumeros = setNumeros.iterator();
 		Scanner ler = new Scanner(System.in);
 		Integer num = Integer.valueOf(10);
 
@@ -18,5 +20,11 @@ public class ListarDados {
 			setNumeros.add(num);
 		}
 		System.out.println("\nLista de dados Set: " + setNumeros);
+		
+		System.out.println("\nLista de dados - Interface Iterator");
+
+		while (isetNumeros.hasNext()) {
+			System.out.println(isetNumeros.next());
+		}
 	}
 }
